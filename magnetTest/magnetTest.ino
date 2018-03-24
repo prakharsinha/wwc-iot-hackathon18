67ac596d9a71a856b0b3388ed2562b4c2a4231e4
Prakhar Sinha <abefaxe@users.noreply.github.com>
2018-03-24 15:27:46 -0700

    Add files via upload

---
+++ magnetTest/magnetTest.ino
@@ -0,0 +1,33 @@
+const int switchPin = 14;
+const int ledPin = 13;
+
+void setup() {
+
+  pinMode(switchPin, INPUT);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  digitalWrite(switchPin, HIGH);
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+Serial.println("loop");
+  if (digitalRead(switchPin) == LOW) {
+
+    digitalWrite(ledPin, LOW);
+    Serial.println("low");
+
+  }
+
+  else {
+
+    digitalWrite(ledPin, HIGH);
+    Serial.println("high");
+
+  }
+
+}
